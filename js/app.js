919a70757c1b36da300f9489c0ed5adea2ca1667
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-03-28 09:27:50 +0700

    ed

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    new WOW().init();
+    // AOS.init({
+    //     duration: 700,
+    //   })
+})
+// $(window).on("load", function () {
+//   $(".preloader").fadeOut(10000);
+//   $(".preloader").remove();
+// });
+
