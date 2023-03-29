33f9f51911b3f638453193fbde4a009828c0c7cd
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-03-29 14:31:16 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-$(document).ready(function () {
-    new WOW().init();
-    // AOS.init({
-    //     duration: 700,
-    //   })
-})
-// $(window).on("load", function () {
-//   $(".preloader").fadeOut(10000);
-//   $(".preloader").remove();
-// });
-
+$(document).ready(function() {
+        new WOW().init();
+        // AOS.init({
+        //     duration: 700,
+        //   })
+    })
+    // $(window).on("load", function () {
+    //   $(".preloader").fadeOut(10000);
+    //   $(".preloader").remove();
+    // });
