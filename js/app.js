ae860230d11ef91ac5d79cd43e4bc54479748e4d
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-03-30 09:40:02 +0700

    in progress

--- js/app.js
+++ js/app.js
@@ -3,8 +3,120 @@ $(document).ready(function() {
         // AOS.init({
         //     duration: 700,
         //   })
+
+       
     })
     // $(window).on("load", function () {
     //   $(".preloader").fadeOut(10000);
     //   $(".preloader").remove();
     // });
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scroll-trigger",
+          pin: true,
+          scrub: 1,
+          start: "top top",
+          end: "+=4000",
+          toggleActions: "play pause play reset"
+        }
+      });
+      
+      tl.from(".title", {
+        opacity: 0,
+        duration: 1
+      })
+        .from(
+          "[data-slide='1'] .image",
+          {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+          },
+          "+=1"
+        )
+        .from(
+          "[data-slide='1'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=1"
+        )
+        .to(
+          "[data-slide='1'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=10"
+        )
+        .to("[data-slide='1'] .image", {
+          opacity: 0,
+          duration: 1
+        })
+        .from(
+          "[data-slide='2'] .image",
+          {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+          },
+          "+=1"
+        )
+        .from(
+          "[data-slide='2'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=1"
+        )
+        .to(
+          "[data-slide='2'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=10"
+        )
+        .to("[data-slide='2'] .image", {
+          opacity: 0,
+          duration: 1
+        })
+        .from(
+          "[data-slide='3'] .image",
+          {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+          },
+          "+=1"
+        )
+        .from(
+          "[data-slide='3'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=1"
+        )
+        .to(
+          "[data-slide='3'] .text",
+          {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+          },
+          "+=10"
+        )
+        .to("[data-slide='3'] .image", {
+          opacity: 0,
+          duration: 1
+        });
+   
