67eacb3bdae565d268bf39b580e4ca1f40cf3947
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-03-31 13:49:04 +0700

    dedededed

--- js/app.js
+++ js/app.js
@@ -1,122 +1,121 @@
-$(document).ready(function() {
-        new WOW().init();
-        // AOS.init({
-        //     duration: 700,
-        //   })
+$(document).ready(function () {
+  new WOW().init();
+  // AOS.init({
+  //     duration: 700,
+  //   })
 
-       
-    })
-    // $(window).on("load", function () {
-    //   $(".preloader").fadeOut(10000);
-    //   $(".preloader").remove();
-    // });
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".scroll-trigger",
-          pin: true,
-          scrub: 1,
-          start: "top top",
-          end: "+=4000",
-          toggleActions: "play pause play reset"
-        }
-      });
-      
-      tl.from(".title", {
-        opacity: 0,
-        duration: 1
-      })
-        .from(
-          "[data-slide='1'] .image",
-          {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-          },
-          "+=1"
-        )
-        .from(
-          "[data-slide='1'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=1"
-        )
-        .to(
-          "[data-slide='1'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=10"
-        )
-        .to("[data-slide='1'] .image", {
-          opacity: 0,
-          duration: 1
-        })
-        .from(
-          "[data-slide='2'] .image",
-          {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-          },
-          "+=1"
-        )
-        .from(
-          "[data-slide='2'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=1"
-        )
-        .to(
-          "[data-slide='2'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=10"
-        )
-        .to("[data-slide='2'] .image", {
-          opacity: 0,
-          duration: 1
-        })
-        .from(
-          "[data-slide='3'] .image",
-          {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-          },
-          "+=1"
-        )
-        .from(
-          "[data-slide='3'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=1"
-        )
-        .to(
-          "[data-slide='3'] .text",
-          {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-          },
-          "+=10"
-        )
-        .to("[data-slide='3'] .image", {
-          opacity: 0,
-          duration: 1
-        });
-   
+})
+// $(window).on("load", function () {
+//   $(".preloader").fadeOut(10000);
+//   $(".preloader").remove();
+// });
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".scroll-trigger",
+    pin: true,
+    scrub: 1,
+    start: "top top",
+    end: "+=4000",
+    toggleActions: "play pause play reset"
+  }
+});
+
+tl.from(".title", {
+  opacity: 0,
+  duration: 1
+})
+  .from(
+    "[data-slide='1'] .image88",
+    {
+      opacity: 0,
+      duration: 1,
+      scale: 0.75
+    },
+    "+=1"
+  )
+  .from(
+    "[data-slide='1'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=1"
+  )
+  .to(
+    "[data-slide='1'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=10"
+  )
+  .to("[data-slide='1'] .image88", {
+    opacity: 0,
+    duration: 1
+  })
+  .from(
+    "[data-slide='2'] .image88",
+    {
+      opacity: 0,
+      duration: 1,
+      scale: 0.75
+    },
+    "+=1"
+  )
+  .from(
+    "[data-slide='2'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=1"
+  )
+  .to(
+    "[data-slide='2'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=10"
+  )
+  .to("[data-slide='2'] .image88", {
+    opacity: 0,
+    duration: 1
+  })
+  .from(
+    "[data-slide='3'] .image88",
+    {
+      opacity: 0,
+      duration: 1,
+      scale: 0.75
+    },
+    "+=1"
+  )
+  .from(
+    "[data-slide='3'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=1"
+  )
+  .to(
+    "[data-slide='3'] .text",
+    {
+      opacity: 0,
+      duration: 1,
+      stagger: 1
+    },
+    "+=10"
+  )
+  .to("[data-slide='3'] .image88", {
+    opacity: 0,
+    duration: 1
+  });
