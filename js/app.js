478f19e4df1f3a9cd7c45c7d0bfbe7cfa81e3902
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-04-03 11:13:36 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,121 +1,112 @@
-$(document).ready(function () {
-  new WOW().init();
-  // AOS.init({
-  //     duration: 700,
-  //   })
+$(document).ready(function() {
+        new WOW().init();
+        // AOS.init({
+        //     duration: 700,
+        //   })
 
 
-})
-// $(window).on("load", function () {
-//   $(".preloader").fadeOut(10000);
-//   $(".preloader").remove();
-// });
+    })
+    // $(window).on("load", function () {
+    //   $(".preloader").fadeOut(10000);
+    //   $(".preloader").remove();
+    // });
 
 const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".scroll-trigger",
-    pin: true,
-    scrub: 1,
-    start: "top top",
-    end: "+=4000",
-    toggleActions: "play pause play reset"
-  }
+    scrollTrigger: {
+        trigger: ".scroll-trigger",
+        pin: true,
+        scrub: 1,
+        start: "top top",
+        end: "+=4000",
+        toggleActions: "play pause play reset"
+    }
 });
 
 tl.from(".title", {
-  opacity: 0,
-  duration: 1
-})
-  .from(
-    "[data-slide='1'] .image88",
-    {
-      opacity: 0,
-      duration: 1,
-      scale: 0.75
-    },
-    "+=1"
-  )
-  .from(
-    "[data-slide='1'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=1"
-  )
-  .to(
-    "[data-slide='1'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=10"
-  )
-  .to("[data-slide='1'] .image88", {
-    opacity: 0,
-    duration: 1
-  })
-  .from(
-    "[data-slide='2'] .image88",
-    {
-      opacity: 0,
-      duration: 1,
-      scale: 0.75
-    },
-    "+=1"
-  )
-  .from(
-    "[data-slide='2'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=1"
-  )
-  .to(
-    "[data-slide='2'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=10"
-  )
-  .to("[data-slide='2'] .image88", {
-    opacity: 0,
-    duration: 1
-  })
-  .from(
-    "[data-slide='3'] .image88",
-    {
-      opacity: 0,
-      duration: 1,
-      scale: 0.75
-    },
-    "+=1"
-  )
-  .from(
-    "[data-slide='3'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=1"
-  )
-  .to(
-    "[data-slide='3'] .text",
-    {
-      opacity: 0,
-      duration: 1,
-      stagger: 1
-    },
-    "+=10"
-  )
-  .to("[data-slide='3'] .image88", {
-    opacity: 0,
-    duration: 1
-  });
+        opacity: 0,
+        duration: 1
+    })
+    .from(
+        "[data-slide='1'] .image88, [data-slide='1'] .image-peta", {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+        },
+        "+=1"
+    )
+    .from(
+        "[data-slide='1'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=1"
+    )
+    .to(
+        "[data-slide='1'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=10"
+    )
+    .to("[data-slide='1'] .image88, [data-slide='1'] .image-peta", {
+        opacity: 0,
+        duration: 1
+    })
+    .from(
+        "[data-slide='2'] .image88", {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+        },
+        "+=1"
+    )
+    .from(
+        "[data-slide='2'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=1"
+    )
+    .to(
+        "[data-slide='2'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=10"
+    )
+    .to("[data-slide='2'] .image88", {
+        opacity: 0,
+        duration: 1
+    })
+    .from(
+        "[data-slide='3'] .image88", {
+            opacity: 0,
+            duration: 1,
+            scale: 0.75
+        },
+        "+=1"
+    )
+    .from(
+        "[data-slide='3'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=1"
+    )
+    .to(
+        "[data-slide='3'] .text", {
+            opacity: 0,
+            duration: 1,
+            stagger: 1
+        },
+        "+=10"
+    )
+    .to("[data-slide='3'] .image88", {
+        opacity: 0,
+        duration: 1
+    });
