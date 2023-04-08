490cc859b6b70354941eb761938addde971cfe68
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-04-09 05:17:25 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-        new WOW().init();
-        // AOS.init({
-        //     duration: 700,
-        //   })
+$(document).ready(function () {
+    new WOW().init();
+    // AOS.init({
+    //     duration: 700,
+    //   })
 
 
-    })
-    // $(window).on("load", function () {
-    //   $(".preloader").fadeOut(10000);
-    //   $(".preloader").remove();
-    // });
+})
+// $(window).on("load", function () {
+//   $(".preloader").fadeOut(10000);
+//   $(".preloader").remove();
+// });
 
 const tl = gsap.timeline({
     scrollTrigger: {
@@ -17,37 +17,37 @@ const tl = gsap.timeline({
         pin: true,
         scrub: 1,
         start: "top top",
-        end: "bottom top",
+        end: "+=4000",
         toggleActions: "play pause play reset"
     }
 });
 
 tl.from(".title", {
-        opacity: 0,
-        duration: 1
-    })
+    opacity: 0,
+    duration: 1
+})
     .from(
         "[data-slide='1'] .image88,[data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
+        opacity: 0,
+        duration: 1,
+        scale: 0.75
+    },
         "+=1"
     )
     .from(
         "[data-slide='1'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=1"
     )
     .to(
         "[data-slide='1'] .text, [data-slide='1'] .slide-title", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=10"
     )
     .to("[data-slide='1'] .image88, [data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
@@ -56,26 +56,26 @@ tl.from(".title", {
     })
     .from(
         "[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
+        opacity: 0,
+        duration: 1,
+        scale: 0.75
+    },
         "+=1"
     )
     .from(
         "[data-slide='2'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=1"
     )
     .to(
         "[data-slide='2'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=10"
     )
     .to("[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
@@ -84,26 +84,26 @@ tl.from(".title", {
     })
     .from(
         "[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
+        opacity: 0,
+        duration: 1,
+        scale: 0.75
+    },
         "+=1"
     )
     .from(
         "[data-slide='3'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=1"
     )
     .to(
         "[data-slide='3'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
+        opacity: 0,
+        duration: 1,
+        stagger: 1
+    },
         "+=10"
     )
     .to("[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
@@ -115,5 +115,4 @@ tl.from(".title", {
 $('.single-item').slick();
 
 
-  
-  
+
