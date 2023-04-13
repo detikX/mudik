5eb8651702c9318a173de57f44fac005116506e4
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-04-13 13:51:22 +0700

    cdc

--- js/app.js
+++ js/app.js
@@ -11,137 +11,137 @@ $(document).ready(function() {
     //   $(".preloader").remove();
     // });
 
-const tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".scroll-trigger",
-        pin: true,
-        scrub: 1,
-        start: "top top",
-        end: "+=4000",
-        toggleActions: "play pause play reset"
-    }
-});
+// const tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".scroll-trigger",
+//         pin: true,
+//         scrub: 1,
+//         start: "top top",
+//         end: "+=4000",
+//         toggleActions: "play pause play reset"
+//     }
+// });
 
-tl.from(".title", {
-        opacity: 0,
-        duration: 1
-    })
-    .from(
-        "[data-slide='1'] .image88,[data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
-        "+=1"
-    )
-    .from(
-        "[data-slide='1'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=1"
-    )
-    .to(
-        "[data-slide='1'] .text, [data-slide='1'] .slide-title", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=10"
-    )
-    .to("[data-slide='1'] .image88, [data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
-        opacity: 0,
-        duration: 1
-    })
-    .from(
-        "[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
-        "+=1"
-    )
-    .from(
-        "[data-slide='2'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=1"
-    )
-    .to(
-        "[data-slide='2'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=10"
-    )
-    .to("[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
-        opacity: 0,
-        duration: 1
-    })
-    .from(
-        "[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
-            opacity: 0,
-            duration: 1,
-            scale: 0.75
-        },
-        "+=1"
-    )
-    .from(
-        "[data-slide='3'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=1"
-    )
-    .to(
-        "[data-slide='3'] .text", {
-            opacity: 0,
-            duration: 1,
-            stagger: 1
-        },
-        "+=10"
-    )
-    .to("[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
-        opacity: 0,
-        duration: 1
-    });
+// tl.from(".title", {
+//         opacity: 0,
+//         duration: 1
+//     })
+//     .from(
+//         "[data-slide='1'] .image88,[data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
+//             opacity: 0,
+//             duration: 1,
+//             scale: 0.75
+//         },
+//         "+=1"
+//     )
+//     .from(
+//         "[data-slide='1'] .text", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=1"
+//     )
+//     .to(
+//         "[data-slide='1'] .text, [data-slide='1'] .slide-title", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=10"
+//     )
+//     .to("[data-slide='1'] .image88, [data-slide='1'] .slide-title, [data-slide='1'] .image-peta", {
+//         opacity: 0,
+//         duration: 1
+//     })
+//     .from(
+//         "[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
+//             opacity: 0,
+//             duration: 1,
+//             scale: 0.75
+//         },
+//         "+=1"
+//     )
+//     .from(
+//         "[data-slide='2'] .text", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=1"
+//     )
+//     .to(
+//         "[data-slide='2'] .text", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=10"
+//     )
+//     .to("[data-slide='2'] .image88, [data-slide='2'] .slide-title, [data-slide='2'] .image-peta", {
+//         opacity: 0,
+//         duration: 1
+//     })
+//     .from(
+//         "[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
+//             opacity: 0,
+//             duration: 1,
+//             scale: 0.75
+//         },
+//         "+=1"
+//     )
+//     .from(
+//         "[data-slide='3'] .text", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=1"
+//     )
+//     .to(
+//         "[data-slide='3'] .text", {
+//             opacity: 0,
+//             duration: 1,
+//             stagger: 1
+//         },
+//         "+=10"
+//     )
+//     .to("[data-slide='3'] .image88, [data-slide='3'] .slide-title, [data-slide='3'] .image-peta", {
+//         opacity: 0,
+//         duration: 1
+//     });
 
 
 $('.single-item').slick();
 
 //kayak faq
 $('.kategori-item').click(function(event) {
-    var id = $(this).attr('id');
-    var toShow = '#show-' + id;
+    var id = $(this).attr('data-id');
+    var toShow = '.show-' + id;
 
     $('.dashboard-content').not(toShow).hide();
     $(toShow).fadeIn();
 });
 
 
-$("#baru-jawa-table").hide()
-$("#fungsi-jawa-table").hide()
-    // $("#biru-jawa").hide();
-$("#biru-jawa-angka").hide();
-$("#kuning-jawa-angka").hide();
+// $("#baru-jawa-table").hide()
+// $("#fungsi-jawa-table").hide()
+//     // $("#biru-jawa").hide();
+// $("#biru-jawa-angka").hide();
+// $("#kuning-jawa-angka").hide();
 // $("#kuning-jawa").hide();
-$("#baru-jawa").click(function() {
-    $("#baru-jawa-table").fadeToggle();
-    // $("#biru-jawa").fadeToggle();
-    $("#biru-jawa-angka").fadeToggle('200');
+// $("#baru-jawa").click(function() {
+//     $("#baru-jawa-table").fadeToggle();
+//     // $("#biru-jawa").fadeToggle();
+//     $("#biru-jawa-angka").fadeToggle('200');
 
-    // setTimeout(function() {
-    //     $('html, body').animate({
-    //         scrollTop: $("#baru-jawa-table").offset().top
-    //     }, 2000);
-    // }, 2000)
+//     // setTimeout(function() {
+//     //     $('html, body').animate({
+//     //         scrollTop: $("#baru-jawa-table").offset().top
+//     //     }, 2000);
+//     // }, 2000)
 
-})
+// })
 $("#fungsi-jawa").click(function() {
     $("#fungsi-jawa-table").fadeToggle();
     // $("#kuning-jawa").fadeToggle();
@@ -154,29 +154,31 @@ $("#fungsi-jawa").click(function() {
 })
 
 
-// $("#sumut-garis-kuning").hide()
-// $("#sumut-garis-biru").hide()
 $("#sumut-no-kuning").hide()
 $("#sumut-no-biru").hide()
-$("#sumut-baru").click(function() {
-        $("#baru-jawa-table").fadeToggle();
-        // $("#sumut-garis-biru").fadeToggle();
-        $("#sumut-no-biru").fadeToggle('200');
+$("#sumut-table-baru").hide();
+$("#sumut-table-fungsi").hide();
+// $("#sumut-no-kuning").hide()
+// $("#sumut-no-biru").hide()
+// $("#sumut-baru").click(function() {
+//         $("#baru-jawa-table").fadeToggle();
+//         // $("#sumut-garis-biru").fadeToggle();
+//         $("#sumut-no-biru").fadeToggle('200');
 
-        // setTimeout(function() {
-        //     $('html, body').animate({
-        //         scrollTop: $("#baru-jawa-table").offset().top
-        //     }, 2000);
-        // }, 2000)
+//         // setTimeout(function() {
+//         //     $('html, body').animate({
+//         //         scrollTop: $("#baru-jawa-table").offset().top
+//         //     }, 2000);
+//         // }, 2000)
 
-    })
-    // $("#sumut-table").hide()
-$("#sumut-fungsi").click(function() {
-    $("#fungsi-sumut-table").fadeToggle();
-    // $("#sumut-garis-kuning").fadeToggle();
-    $("#sumut-no-kuning").fadeToggle('200'); // setTimeout(function() {
-    //     $('html, body').animate({
-    //         scrollTop: $("#fungsi-jawa-table").offset().top
-    //     }, 2000);
-    // }, 2000)
-})
+//     })
+//     // $("#sumut-table").hide()
+// $("#sumut-fungsi").click(function() {
+//     $("#fungsi-sumut-table").fadeToggle();
+//     // $("#sumut-garis-kuning").fadeToggle();
+//     $("#sumut-no-kuning").fadeToggle('200'); // setTimeout(function() {
+//     //     $('html, body').animate({
+//     //         scrollTop: $("#fungsi-jawa-table").offset().top
+//     //     }, 2000);
+//     // }, 2000)
+// })
