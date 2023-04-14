c57d6aeed0f1fa0d94de3b08dadf1b7b75528669
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-04-14 13:25:38 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -190,3 +190,42 @@ $("#sumut-table-fungsi").hide();
 //     //     }, 2000);
 //     // }, 2000)
 // })
+
+
+
+$(function() {
+    function isScrolledIntoView($elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    function count($this) {
+        var current = parseInt($this.html(), 10);
+        if (isScrolledIntoView($this) && !$this.data("isCounting") && current < $this.data('count')) {
+            $this.html(++current);
+            $this.data("isCounting", true);
+            setTimeout(function() {
+                $this.data("isCounting", false);
+                count($this);
+            }, 1);
+        }
+    }
+
+    $(".count").each(function() {
+        $(this).data('count', parseInt($(this).html(), 10));
+        $(this).html('1900');
+        $(this).data("isCounting", false);
+    });
+
+    function startCount() {
+        $(".count").each(function() {
+            count($(this));
+        });
+    };
+    $(window).scroll(function() {
+        startCount();
+    });
+});
